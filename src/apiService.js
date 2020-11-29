70ef54710f6187203f1cc6e3ca86ac68681e6390
Oksana-Hubenko89 <65037336+Oksana-Hubenko89@users.noreply.github.com>
2020-11-30 00:46:05 +0200

    добавление дз на github

---
+++ src/apiService.js
@@ -0,0 +1,49 @@
+import { Utils } from "handlebars";
+
+const API_KEY = '19267530-a7778ead7c20bf1fd9d6edf89';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default class NewsApiService {
+
+    constructor() {
+    this.searchQuery = '';
+    this.page = 1;       
+    }
+    
+  async  fetchArticles() {
+    const searchParams=new URLSearchParams({
+      image_type:this.photo,
+      orientation:this.horizontal,
+      q:this.searchQuery,
+      page:this.page,
+      per_page:12,
+    })
+    const url=`${BASE_URL}?${searchParams}&key=${API_KEY}`
+   //const url=`${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}=&page=${this.page}&per_page=12&key=${API_KEY}`;
+        return await fetch(url)
+            .then(response => response.json())
+            .then(data => {
+               // console.log(data);               
+                return data.hits;
+            })
+            ;
+    }
+
+   
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
+ 
